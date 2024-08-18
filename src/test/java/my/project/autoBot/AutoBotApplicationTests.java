d31c2ac50b23c2f8b13757897f54869b95f72538
Mark7631 <lmark.gm@gmail.com>
2024-08-18 12:36:33 +0300

    main: first commit

---
+++ src/test/java/my/project/autoBot/AutoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.autoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
